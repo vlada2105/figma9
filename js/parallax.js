1036d8fbb35882f2e0b56fbb7221fd8a7b7b5210
lada2105 <spar120@mail.ru>
2024-11-05 19:07:04 +0300

    toTop button

--- js/parallax.js
+++ js/parallax.js
@@ -59,7 +59,9 @@ $(function() {
 	});
 });
 // скролл menu active
-var sections = $('.level')
+	// где id="..."
+	var sections = $('.level')
+	// где ссылка a href="#..."
 	, nav = $('nav')
 	, nav_height = nav.outerHeight();
 
@@ -129,3 +131,21 @@ nav2.find('a').on('click', function () {
 
 	return false;
 });
+
+
+// to Top Button
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		document.getElementById("toTop").style.display = "block";
+	} else {
+		document.getElementById("toTop").style.display = "none";
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
