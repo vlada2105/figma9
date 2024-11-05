62e849a1a85f7fa594d75b2b347e83ebb3367a49
lada2105 <spar120@mail.ru>
2024-11-05 18:46:02 +0300

    fixed header, scale scroll

--- js/parallax.js
+++ js/parallax.js
@@ -58,3 +58,74 @@ $(function() {
 		}
 	});
 });
+// скролл menu active
+var sections = $('.level')
+	, nav = $('nav')
+	, nav_height = nav.outerHeight();
+
+	$(window).on('scroll', function () {
+		var cur_pos = $(this).scrollTop();
+
+		sections.each(function() {
+		var top = $(this).offset().top - nav_height,
+			bottom = top + $(this).outerHeight();
+
+			if (cur_pos >= top && cur_pos <= bottom) {
+				nav.find('a').removeClass('active');
+				sections.removeClass('active');
+				
+				$(this).addClass('active');
+				nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+			}
+			if (window.scrollY < 400) {
+				nav.find('a[href="#'+$(this).attr('id')+'"]').removeClass('active');
+			} 
+		});
+});
+
+nav.find('a').on('click', function () {
+	var $el = $(this)
+	, id = $el.attr('href');
+
+	$('html, body').animate({
+	scrollTop: $(id).offset().top - nav_height
+	}, 500);
+
+	return false;
+});
+// скролл scale
+var sections2 = $('.scale')
+	, nav2 = $('.mainscreen__scale')
+	, nav_height2 = nav2.outerHeight();
+
+	$(window).on('scroll', function () {
+		var cur_pos2 = $(this).scrollTop();
+
+		sections2.each(function() {
+		var top2 = $(this).offset().top - nav_height2,
+			bottom2 = top2 + $(this).outerHeight();
+
+			if (cur_pos2 >= top2 && cur_pos2 <= bottom2) {
+				nav2.find('a').removeClass('active');
+				sections2.removeClass('active');
+				
+				$(this).addClass('active');
+				nav2.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+			}
+			if (window.scrollY < 400) {
+				nav2.find('a[href="#'+$(this).attr('id')+'"]').removeClass('active');
+			} 
+
+		});
+});
+
+nav2.find('a').on('click', function () {
+	var $el2 = $(this)
+	, id2 = $el2.attr('href');
+
+	$('html, body').animate({
+	scrollTop: $(id2).offset().top2 - nav_height2
+	}, 500);
+
+	return false;
+});
